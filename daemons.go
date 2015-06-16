01fd7c1773ecf4b8a3401d6720d76af4aef8fc64
elgs <elgs1980@hotmail.com>
2015-06-16 21:33:45 +0800

    Update.

--- daemons.go
+++ daemons.go
@@ -5,16 +5,60 @@ import (
 	"crypto/tls"
 	"fmt"
 	"github.com/elgs/gorest2"
+	"github.com/elgs/gosqljson"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	gorest2.RegisterJob("dummy", &gorest2.Job{
 		Cron: "0 0 * * * *",
 		MakeAction: func(dbo gorest2.DataOperator) func() {
 			return func() {
 
+			}
+		},
+	})
+
+	gorest2.RegisterJob("invalidate_token", &gorest2.Job{
+		Cron: fmt.Sprint(rand.Intn(60), " * * * * *"),
+		MakeAction: func(dbo gorest2.DataOperator) func() {
+			lastUpdateSince := ""
+			return func() {
+				db, err := dbo.GetConn()
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				if lastUpdateSince == "" {
+					// minus the interval of this job (1 min) to eliminate the tokens changed before this job first run.
+					changedTokens, err := gosqljson.QueryDbToMap(db, "upper",
+						"SELECT UPDATE_TIME - INTERVAL 1 MINUTE AS UPDATE_TIME FROM token ORDER BY UPDATE_TIME DESC LIMIT 1")
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					lastChangedToken := changedTokens[0]
+					lastUpdateSince = lastChangedToken["UPDATE_TIME"]
+				} else {
+					changedTokens, err := gosqljson.QueryDbToMap(db, "upper",
+						"SELECT TOKEN,UPDATE_TIME FROM token WHERE UPDATE_TIME>? ORDER BY UPDATE_TIME DESC", lastUpdateSince)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					for i, changedToken := range changedTokens {
+						delete(projectTokenRegistry, changedToken["TOKEN"])
+						if i == 0 {
+							lastUpdateSince = changedToken["UPDATE_TIME"]
+						}
+					}
+				}
 			}
 		},
 	})
