3b16b4ab66f09dd9e086efce4a66983e90eb4cd4
elgs <elgs1980@hotmail.com>
2015-06-20 18:07:09 +0800

    Update.

--- global_token_interceptor.go
+++ global_token_interceptor.go
@@ -209,20 +209,47 @@ func (this *GlobalTokenInterceptor) AfterListArray(resourceId string, db *sql.DB
 	return nil
 }
 func (this *GlobalTokenInterceptor) BeforeQueryMap(resourceId string, db *sql.DB, context map[string]interface{}, start int64, limit int64, includeTotal bool) (bool, error) {
-	return true, nil
+	if !isDefaultProjectRequest(context) {
+		return true, nil
+	}
+	if ok, err := checkACL(resourceId, "query"); !ok {
+		return false, err
+	}
+	return checkDefaultToken(db, context["token"].(string), context, resourceId)
 }
 func (this *GlobalTokenInterceptor) AfterQueryMap(resourceId string, db *sql.DB, context map[string]interface{}, data []map[string]string, total int64) error {
+	if !isDefaultProjectRequest(context) {
+		return nil
+	}
 	return nil
 }
 func (this *GlobalTokenInterceptor) BeforeQueryArray(resourceId string, db *sql.DB, context map[string]interface{}, start int64, limit int64, includeTotal bool) (bool, error) {
-	return true, nil
+	if !isDefaultProjectRequest(context) {
+		return true, nil
+	}
+	if ok, err := checkACL(resourceId, "query"); !ok {
+		return false, err
+	}
+	return checkDefaultToken(db, context["token"].(string), context, resourceId)
 }
 func (this *GlobalTokenInterceptor) AfterQueryArray(resourceId string, db *sql.DB, context map[string]interface{}, headers []string, data [][]string, total int64) error {
+	if !isDefaultProjectRequest(context) {
+		return nil
+	}
 	return nil
 }
 func (this *GlobalTokenInterceptor) BeforeExec(resourceId string, db *sql.DB, context map[string]interface{}) (bool, error) {
-	return true, nil
+	if !isDefaultProjectRequest(context) {
+		return true, nil
+	}
+	if ok, err := checkACL(resourceId, "exec"); !ok {
+		return false, err
+	}
+	return checkDefaultToken(db, context["token"].(string), context, resourceId)
 }
 func (this *GlobalTokenInterceptor) AfterExec(resourceId string, db *sql.DB, context map[string]interface{}) error {
+	if !isDefaultProjectRequest(context) {
+		return nil
+	}
 	return nil
 }
--- global_token_project_interceptor.go
+++ global_token_project_interceptor.go
@@ -193,20 +193,38 @@ func (this *GlobalTokenProjectInterceptor) AfterListArray(resourceId string, db 
 	return nil
 }
 func (this *GlobalTokenProjectInterceptor) BeforeQueryMap(resourceId string, db *sql.DB, context map[string]interface{}, start int64, limit int64, includeTotal bool) (bool, error) {
-	return true, nil
+	if isDefaultProjectRequest(context) {
+		return true, nil
+	}
+	return checkProjectToken(context["app_id"].(string), context["token"].(string), resourceId, "rx")
 }
 func (this *GlobalTokenProjectInterceptor) AfterQueryMap(resourceId string, db *sql.DB, context map[string]interface{}, data []map[string]string, total int64) error {
+	if isDefaultProjectRequest(context) {
+		return nil
+	}
 	return nil
 }
 func (this *GlobalTokenProjectInterceptor) BeforeQueryArray(resourceId string, db *sql.DB, context map[string]interface{}, start int64, limit int64, includeTotal bool) (bool, error) {
-	return true, nil
+	if isDefaultProjectRequest(context) {
+		return true, nil
+	}
+	return checkProjectToken(context["app_id"].(string), context["token"].(string), resourceId, "rx")
 }
 func (this *GlobalTokenProjectInterceptor) AfterQueryArray(resourceId string, db *sql.DB, context map[string]interface{}, headers []string, data [][]string, total int64) error {
+	if isDefaultProjectRequest(context) {
+		return nil
+	}
 	return nil
 }
 func (this *GlobalTokenProjectInterceptor) BeforeExec(resourceId string, db *sql.DB, context map[string]interface{}) (bool, error) {
-	return true, nil
+	if isDefaultProjectRequest(context) {
+		return true, nil
+	}
+	return checkProjectToken(context["app_id"].(string), context["token"].(string), resourceId, "wx")
 }
 func (this *GlobalTokenProjectInterceptor) AfterExec(resourceId string, db *sql.DB, context map[string]interface{}) error {
+	if isDefaultProjectRequest(context) {
+		return nil
+	}
 	return nil
 }
