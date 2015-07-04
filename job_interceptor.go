ad172bfd39867fd47ad8868eb1b58963a6e844f7
elgs <elgs1980@hotmail.com>
2015-07-04 17:02:47 +0800

    Update.

--- job_interceptor.go
+++ job_interceptor.go
@@ -16,19 +16,26 @@ type JobInterceptor struct {
 }
 
 func (this *JobInterceptor) BeforeUpdate(resourceId string, db *sql.DB, context map[string]interface{}, data map[string]interface{}) (bool, error) {
-	context["load"] = true
 	return true, nil
 }
-
 func (this *JobInterceptor) AfterUpdate(resourceId string, db *sql.DB, context map[string]interface{}, data map[string]interface{}) error {
 	return nil
 }
-
 func (this *JobInterceptor) BeforeDelete(resourceId string, db *sql.DB, context map[string]interface{}, id string) (bool, error) {
-	context["load"] = true
 	return true, nil
 }
-
 func (this *JobInterceptor) AfterDelete(resourceId string, db *sql.DB, context map[string]interface{}, id string) error {
+	return nil
+}
+func (this *JobInterceptor) BeforeListMap(resourceId string, db *sql.DB, fields string, context map[string]interface{}, filter *string, sort *string, group *string, start int64, limit int64, includeTotal bool) (bool, error) {
+	return true, nil
+}
+func (this *JobInterceptor) AfterListMap(resourceId string, db *sql.DB, fields string, context map[string]interface{}, data []map[string]string, total int64) error {
+	return nil
+}
+func (this *JobInterceptor) BeforeListArray(resourceId string, db *sql.DB, fields string, context map[string]interface{}, filter *string, sort *string, group *string, start int64, limit int64, includeTotal bool) (bool, error) {
+	return true, nil
+}
+func (this *JobInterceptor) AfterListArray(resourceId string, db *sql.DB, fields string, context map[string]interface{}, headers []string, data [][]string, total int64) error {
 	return nil
 }
