01074243980beb0984211004edc3001bf878a4be
elgs <elgs1980@hotmail.com>
2015-11-28 19:06:53 -0800

    Disabled auto refresh data in the web page.

--- global_data_interceptor.go
+++ global_data_interceptor.go
@@ -15,24 +15,20 @@ type GlobalDataInterceptor struct {
 }
 
 func (this *GlobalDataInterceptor) AfterCreate(resourceId string, db *sql.DB, context map[string]interface{}, data map[string]interface{}) error {
-	wsMsgQueue <- 0
+	//	wsMsgQueue <- 0
 	return nil
 }
 
 func (this *GlobalDataInterceptor) AfterUpdate(resourceId string, db *sql.DB, context map[string]interface{}, data map[string]interface{}) error {
-	wsMsgQueue <- 0
+	//	wsMsgQueue <- 0
 	return nil
 }
 
 func (this *GlobalDataInterceptor) AfterDuplicate(resourceId string, db *sql.DB, context map[string]interface{}, id string, newId string) error {
-	wsMsgQueue <- 0
+	//	wsMsgQueue <- 0
 	return nil
 }
 func (this *GlobalDataInterceptor) AfterDelete(resourceId string, db *sql.DB, context map[string]interface{}, id string) error {
-	wsMsgQueue <- 0
-	return nil
-}
-func (this *GlobalDataInterceptor) AfterExec(resourceId string, scripts string, params []interface{}, tx *sql.Tx, context map[string]interface{}) error {
-	wsMsgQueue <- 0
+	//	wsMsgQueue <- 0
 	return nil
 }
