faf8be95c56d30e9e44ffa54ab33adb6a8912434
elgs <elgs1980@hotmail.com>
2015-12-25 03:40:27 -0800

    Update.

--- global_remote_interceptor.go
+++ global_remote_interceptor.go
@@ -3,12 +3,14 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/elgs/gorest2"
 )
 
 func init() {
 	loadACL()
 	gorest2.RegisterGlobalDataInterceptor(30, &GlobalRemoteInterceptor{Id: "GlobalRemoteInterceptor"})
+	loadAllRemoteInterceptor()
 }
 
 var RemoteInterceptorRegistry = map[string]*RemoteInterceptorDefinition{}
@@ -27,167 +29,186 @@ type RemoteInterceptorDefinition struct {
 	ActionType string // create, load, update, delete, ...
 }
 
-func (this *GlobalRemoteInterceptor) loadRemoteInterceptor(projectId, target, theType, actionType string) *RemoteInterceptorDefinition {
+func loadAllRemoteInterceptor() error {
+	// load all remote interceptor definitions into RemoteInterceptorRegistry
+	return nil
+}
+
+func loadRemoteInterceptor(projectId, target, theType, actionType string) error {
+	// load specific remote interceptor definitions into RemoteInterceptorRegistry
+	return nil
+}
+
+func unloadRemoteInterceptor(projectId, target, theType, actionType string) error {
+	// unload specific remote interceptor definitions into RemoteInterceptorRegistry
+	return nil
+}
+
+func (this *GlobalRemoteInterceptor) checkAgainstBeforeRemoteInterceptor(ri *RemoteInterceptorDefinition) (bool, error) {
+	return true, nil
+}
+
+func (this *GlobalRemoteInterceptor) checkAgainstAfterRemoteInterceptor(ri *RemoteInterceptorDefinition) error {
 	return nil
 }
 
 func (this *GlobalRemoteInterceptor) BeforeCreate(resourceId string, db *sql.DB, context map[string]interface{}, data map[string]interface{}) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "create")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"create"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "create")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterCreate(resourceId string, db *sql.DB, context map[string]interface{}, data map[string]interface{}) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "create")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"create"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "create")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeLoad(resourceId string, db *sql.DB, fields string, context map[string]interface{}, id string) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "load")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"load"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "load")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterLoad(resourceId string, db *sql.DB, fields string, context map[string]interface{}, data map[string]string) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "load")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"load"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "load")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeUpdate(resourceId string, db *sql.DB, context map[string]interface{}, data map[string]interface{}) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "update")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"update"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "update")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterUpdate(resourceId string, db *sql.DB, context map[string]interface{}, data map[string]interface{}) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "update")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"update"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "update")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeDuplicate(resourceId string, db *sql.DB, context map[string]interface{}, id string) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "duplicate")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"duplicate"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "duplicate")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterDuplicate(resourceId string, db *sql.DB, context map[string]interface{}, id string, newId string) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "duplicate")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"duplicate"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "duplicate")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeDelete(resourceId string, db *sql.DB, context map[string]interface{}, id string) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "delete")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"delete"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "delete")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterDelete(resourceId string, db *sql.DB, context map[string]interface{}, id string) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "delete")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"delete"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "delete")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeListMap(resourceId string, db *sql.DB, fields string, context map[string]interface{}, filter *string, sort *string, group *string, start int64, limit int64) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "listmap")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"listmap"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "list_map")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterListMap(resourceId string, db *sql.DB, fields string, context map[string]interface{}, data *[]map[string]string, total int64) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "listmap")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"listmap"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "list_map")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeListArray(resourceId string, db *sql.DB, fields string, context map[string]interface{}, filter *string, sort *string, group *string, start int64, limit int64) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "listarray")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"listarray"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "list_array")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterListArray(resourceId string, db *sql.DB, fields string, context map[string]interface{}, headers *[]string, data *[][]string, total int64) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "listarray")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"listarray"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "list_array")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeQueryMap(resourceId string, script string, params *[]interface{}, db *sql.DB, context map[string]interface{}) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "querymap")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"querymap"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "query_map")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterQueryMap(resourceId string, script string, params *[]interface{}, db *sql.DB, context map[string]interface{}, data *[]map[string]string) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "querymap")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"querymap"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "query_map")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeQueryArray(resourceId string, script string, params *[]interface{}, db *sql.DB, context map[string]interface{}) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "queryarray")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"queryarray"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "query_array")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterQueryArray(resourceId string, script string, params *[]interface{}, db *sql.DB, context map[string]interface{}, headers *[]string, data *[][]string) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "queryarray")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"queryarray"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "query_array")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) BeforeExec(resourceId string, scripts string, params *[]interface{}, tx *sql.Tx, context map[string]interface{}) (bool, error) {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "before", "exec")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"before"+"exec"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "before", "exec")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return true, nil
 	}
-	return true, nil
+	return this.checkAgainstBeforeRemoteInterceptor(ri)
 }
 func (this *GlobalRemoteInterceptor) AfterExec(resourceId string, scripts string, params *[]interface{}, tx *sql.Tx, context map[string]interface{}) error {
-	projectId := context["app_id"].(string)
-	ri := this.loadRemoteInterceptor(projectId, resourceId, "after", "exec")
-	if ri != nil {
-		RemoteInterceptorRegistry[projectId+resourceId+"after"+"exec"] = ri
+	key := fmt.Sprint(context["app_id"], resourceId, "after", "exec")
+	ri := RemoteInterceptorRegistry[key]
+	if ri == nil {
+		return nil
 	}
-	return nil
+	return this.checkAgainstAfterRemoteInterceptor(ri)
 }
