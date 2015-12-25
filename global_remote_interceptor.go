51106544b232214c45a1f2cc8364396263e39dea
elgs <elgs1980@hotmail.com>
2015-12-25 02:14:53 -0800

    Update.

--- global_remote_interceptor.go
+++ global_remote_interceptor.go
@@ -1,4 +1,16 @@
 // global_remote_interceptor
 package main
 
-import ()
+import (
+	"github.com/elgs/gorest2"
+)
+
+func init() {
+	loadACL()
+	gorest2.RegisterGlobalDataInterceptor(30, &GlobalRemoteInterceptor{Id: "GlobalRemoteInterceptor"})
+}
+
+type GlobalRemoteInterceptor struct {
+	*gorest2.DefaultDataInterceptor
+	Id string
+}
