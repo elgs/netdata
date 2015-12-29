3df05de9c5f2059126601c5adc0bd5604ce4e34c
elgs <elgs1980@hotmail.com>
2015-12-28 19:22:33 -0800

    Update.

--- limitedReadCloser.go
+++ limitedReadCloser.go
@@ -12,7 +12,7 @@ type LimitedReadCloser struct {
 }
 
 func (l *LimitedReadCloser) Read(p []byte) (n int, err error) {
-	if l.N < 0 {
+	if l.N <= 0 {
 		return 0, errors.New("http: response body too large")
 	}
 	if int64(len(p)) > l.N {
